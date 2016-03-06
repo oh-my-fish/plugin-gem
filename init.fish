bc4fc5f6899c86937c9d7d9a9446732c5c19db9f
Justin Hileman <justin@justinhileman.info>
2016-03-06 13:08:07 -0800

    Initialize immediately, rather than waiting for an event.

--- init.fish
+++ init.fish
@@ -1,8 +1,6 @@
-function init --on-event init_gem
-  if test -n "$GEM_ROOT"
-    set -gx GEM_HOME $GEM_ROOT
-    if not contains $GEM_ROOT/bin $PATH
-      set -x PATH $GEM_ROOT/bin $PATH
-    end
+if test -n "$GEM_ROOT"
+  set -gx GEM_HOME $GEM_ROOT
+  if not contains $GEM_ROOT/bin $PATH
+    set -x PATH $GEM_ROOT/bin $PATH
   end
 end
